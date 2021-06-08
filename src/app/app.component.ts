1de606175e2e10b038fb9caa406484de7dd29f24
CheyenneABounds <61998423+CheyenneABounds@users.noreply.github.com>
2021-06-07 19:31:01 -0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    template: '<h1>My App</h1>' 
+    selector: 'mw-app',
+    template: '<h1>My App</h1><p>Keeping track of the media I watch</p>' 
 })
 export class AppComponent {}
