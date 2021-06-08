a016c26eda022e925fbdd90e06676e8e10f0949a
CheyenneABounds <61998423+CheyenneABounds@users.noreply.github.com>
2021-06-08 09:41:48 -0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'mw-app',
     templateUrl: './app.component.html',
-    styles: ['./app.component.css']
+    styles: ['./app.component.css'],
 })
 export class AppComponent {}
