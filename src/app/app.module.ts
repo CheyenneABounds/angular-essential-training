b438984daa8e19f1a8b5ab570982ae2955b1856f
CheyenneABounds <61998423+CheyenneABounds@users.noreply.github.com>
2021-06-09 09:46:51 -0700

    added HTTP file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
@@ -8,11 +10,13 @@ import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
 import { lookupListToken, lookupLists } from './providers';
+import { MockXHRBackend } from './mock-xhr-backend';
 
 @NgModule({
     imports: [
         BrowserModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        HttpClientModule
     ],
     declarations: [
         AppComponent,
@@ -23,7 +27,8 @@ import { lookupListToken, lookupLists } from './providers';
         MediaItemFormComponent
     ],
     providers: [
-        { provide: lookupListToken, useValue: lookupLists }
+        { provide: lookupListToken, useValue: lookupLists },
+        { provide: HttpXhrBackend, useClass: MockXHRBackend }
     ],
     bootstrap: [
         AppComponent
